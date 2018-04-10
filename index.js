574470664659ad7fe34cc62f10a25a87bba2e0bb
vverheij <v.verheij@gmail.com>
2018-04-11 00:01:05 +0200

    wip

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const express = require('express');
+
 const Joi = require('joi');
 const logger = require('./logger');
 const authenticator = require('./authenticator');
 const app = express();
+
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+console.log(`env: ${app.get('env')}`);
 
 app.use(express.json());
 app.use(logger);
@@ -11,7 +16,11 @@ app.use(logger);
 app.use(authenticator);
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
-app.use(morgan('tiny'));
+
+if (app.get('env') == 'development') {
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled');    
+}
 
 const courses = [
     {
