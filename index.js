d9625a7b0c1f3507c498e57fefdc2eebd4ae7d94
Victor Verheij <v.verheij@gmail.com>
2018-04-15 12:59:03 +0200

    working again

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const config = require('config');
 const morgan = require('morgan');
 const helmet = require('helmet');
@@ -31,10 +32,10 @@ console.log(`Application Name: ${config.get('name')}`);
 console.log(`Mail Server: ${config.get('mail.host')}`);
 console.log(`Mail Password: ${config.get('mail.password')}`);
 
-// if (app.get('env') == 'development') {
-//     app.use(morgan('tiny'));
-//     console.log('Morgan enabled');    
-// }
+if (app.get('env') == 'development') {
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled');    
+}
 
 const port = process.env.PORT || 3000;
 
